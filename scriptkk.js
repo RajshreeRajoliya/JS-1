af7bbad89b5e8ebf994db73d349d4a95cead6cce
RajshreeRajoliya <rajshreerajoliya14@gmail.com>
2024-06-04 23:47:24 +0530

    pr

---
+++ scriptkk.js
@@ -0,0 +1,43 @@
+// Array.prototype.Rajshree = function(cb){
+//     let newArray = []
+//     for(let i = 0 ; i < this.length ; i++){
+// newArray.push(cb(this[i]))
+//     }
+//     return newArray
+// }
+
+// let arr = [1,2,3,4,5]
+// let ans = arr.Rajshree((el)=>{
+//     return el * 5
+// })
+// console.log(ans)
+
+
+
+
+
+
+// Array.prototype.Rajshree = function(cb){
+//     let newArray = []
+//     for(let i = 0 ; i < this.length ; i++){
+//         if(cb(this[i])){
+// newArray.push(this[i])
+//         }
+//     }
+//     return newArray
+// }
+
+// let arr = [1 , 2 , 3 ,4]
+// let ans = arr.Rajshree((el)=>{
+//     return el % 2 == 0
+// })
+// console.log(ans)
+
+
+
+Array.prototype.Rajshree = function(acc , curr){
+    let newArray = []
+    for(let i = 0 ; i < this.length ; i++){
+newArray.push()
+    }
+}
