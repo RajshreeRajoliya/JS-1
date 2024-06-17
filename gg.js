1d66f0a8cc86bf5d32b00aeff241f749fe0a3aed
RajshreeRajoliya <rajshreerajoliya14@gmail.com>
2024-06-17 15:13:23 +0530

    s

---
+++ gg.js
@@ -0,0 +1,26 @@
+ for(var i = 0 ; i < 5 ; i++){
+    setTimeout(()=>{
+console.log(i)
+    },i * 1000)
+}
+
+for(let i = 0 ; i < 5 ; i++){
+    setTimeout(()=>{
+console.log(i)
+    },i * 1000)
+}
+
+
+// var a = 90
+// {
+//     var a = 98
+//     console.log(a)
+// }
+// console.log(a)
+
+// let a = 90
+// {
+//     let a = 98
+//     console.log(a)
+// }
+// console.log(a)
